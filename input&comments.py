f68141cbcc2a76166bf6a2279253a3789ae3c013
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-09-13 02:33:14 +0600

    Add new lines

--- input&comments.py
+++ input&comments.py
@@ -1,3 +1,7 @@
 # In python # is used for comments in python
 Name = input("Enter your name :")
 print(Name) #This is use for print somthing
+"""floor(value)---> give closest integer value  result A//B,floor(A/B) same
+ 1.Reassigning a variable unlinks the old value from memory.
+ 2.Python's garbage collector automatically clears unreferenced values.
+ 3.Python is implicitly typed, determining data types without explicit declarations."""
