9ccdb4fb31f22dd0a686686440281098d7570701
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-09-13 03:37:08 +0600

    add new line

--- input&comments.py
+++ input&comments.py
@@ -1,7 +1,15 @@
 # In python # is used for comments in python
-Name = input("Enter your name :")
-print(Name) #This is use for print somthing
+# Name = input("Enter your name :")
+# print(Name) #This is use for print somthing
 """1.floor(value)---> give closest integer value  result A//B,floor(A/B) same
    2."in" is a membership operator.
    3.variable declaration is implicit in python.
    4.operator precedance not > and > or"""
+
+name = input("Enter Name :")
+age = int(input("Enter Age :"))
+marks = int(input("Enter marks :"))
+
+print("welcome :",name)
+print("age :",age, type(age))
+print("marks :",marks)
