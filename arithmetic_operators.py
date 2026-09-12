48d16522a9c16c75200fbf90405f84b137aa7a84
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-09-13 03:11:25 +0600

    add new word in comment section

--- arithmetic_operators.py
+++ arithmetic_operators.py
@@ -4,4 +4,4 @@ print( 5 * 3)
 print( 5 / 3)
 print( 5 // 3) # it remove the values which are after point
 print( 5 % 3)
-print( 5 ** 3 ) # it is use for make power
+print( 5 ** 3 ) # it is use for make power suppose a^b
