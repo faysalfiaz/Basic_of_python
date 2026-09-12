98a151ff49f20871303c0c1a391d4a629edd84a4
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-09-13 02:31:22 +0600

    Add new lines

--- DataType.py
+++ DataType.py
@@ -2,4 +2,17 @@ Name = "Ramim Rizwan";
 print(type(Name));
 Age = 23;
 print(type(Age));
+a = None 
+print(type(a))
 # we can know the type of variable using this funcation 
+print("My name is Faysal Ahmed Ramim.","I am a student")
+# this is print in same line
+print("your another name is :", Name)
+name1 ='sk'
+name2 = "sk"
+name3 = '''sk'''
+print(name1)
+print(name2)
+print(name3)
+
+# python: An implicitly typed language is a programming language where the compiler or interpreter automatically determines a variable's data type without requiring you to declare it explicitly.
