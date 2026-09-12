2c1e67e0ce433394a2f91106782c41581a424de5
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-09-13 03:25:45 +0600

    add comments

--- typeconvertion.py
+++ typeconvertion.py
@@ -1,5 +1,5 @@
 # typecasting and type conservation
-#select and ctrl + / we can easily comment 
+# select and ctrl + / we can easily comment 
 # age = input("Enter your Age :")
 # print(age)
 # new_age = int(age) + 1;
@@ -9,7 +9,11 @@
 # sum = a + b 
 # print(sum)
 # string is immutable --> if we create a string we can not change it ,, every time it create new string for new operation
+# string cannot convert in float 
 name  = "Faysal Ahmed Ramim"
 print(name.upper())
 #find
 print(name.find("mim")) # it is return index
+
+# Type conversion happens automatically by the programming language when a value of one data type is shifted to another.
+# Type casting is done manually by the programmer using explicit syntax to force a value into a specific data type.
