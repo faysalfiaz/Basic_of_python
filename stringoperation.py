223196518939588752fea89cf2a8d6bd71874afa
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-09-13 21:41:35 +0600

    add new lines

--- stringoperation.py
+++ stringoperation.py
@@ -1,5 +1,14 @@
-# replace 
+# string
+str1 = "This is string"
+str2 = 'hello'
+str3 = """This is another"""
 
+# newline  /n
+# tab /t
+# concatinate -->str1+str2
+# using index we cannot change the value
+
+# replace 
 name = "Faysal"
 print(name.replace("Faysal","Thor"))
 own_name = "Ramim"
