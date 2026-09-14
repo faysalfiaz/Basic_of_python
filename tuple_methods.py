cdd7978cedcbd7e6b38a777ff0ef693e6f01b0db
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-09-14 23:59:16 +0600

    add new file

---
+++ tuple_methods.py
@@ -0,0 +1,9 @@
+tup = (2,1,3,1)
+tup.index(2)  # it returns index
+tup.count(2)   # it returns total occurrences
+
+movies = []
+movies.append(input ("Enter 1st movie:"))
+movies.append(input ("Enter 2nd movie:"))
+movies.append(input ("Enter 3rd movie:"))
+print(movies)
