656ab42017c6d6376fe884e7d19a5d5dc61a94ac
Faysal Ahmed Ramim <faysal15961@gmail.com>
2026-09-14 23:58:28 +0600

    add some lines

--- tuple.py
+++ tuple.py
@@ -1,8 +1,9 @@
-#tuple is immutable --> we cannot change it if we create once
+# tuple is immutable --> we cannot change it if we create once
 marks = (10,50,30,45,90,79)
-
+# we cannot change value by using index
 print(marks,type(marks))
 
+# tup = (1,)!! if we donot use , then it is taken as int
 # print using index
 print(marks[2])
 
@@ -11,3 +12,5 @@ print(marks.count(10))
 
 # print index
 print(marks.index(50))
+
+print(marks[1:3])
