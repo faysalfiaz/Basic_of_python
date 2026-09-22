15578b98166d46ba56dfa11393ae160dd1e433a3
faysalfiaz <faysal15961@gmail.com>
2026-09-21 22:08:46 -0700

    add some line

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,47 @@
 # dictionary {key ===> value}
 # there have no index
-marks = {"math":99, "physics":97,"chemistry":99}
-print(marks,type(marks))
-# change value
-marks["physics"] = 100
-print(marks["physics"])
+# we can change value (mutable)
+# donot allow duplicate keys
 
-# print using loops
-for key in marks :
-    print(key,marks[key])
+info ={
+    "key":"value",
+    "name":"faysal_Ahmed",
+    "learning":"python",
+    "age":35,
+    "is_adult":True,
+    "maks":94.4,
+    "subject":["python","c","java"],
+    "topics":("dict","set"),
+}
+# print(info["name"])
+
+
+# change value
+info["name"] = "Faysal Ahmed Ramim"
+# null dictionary
+null_dict = {}
+null_dict["name"] ="FaysalFaiz"
+# print(null_dict)
+
+
+# nested dict 
+student = {
+    "name":"Ramim Rizwan",
+    "subject":{
+        "phy":51,
+        "chem":30,
+        "math":25
+    }
+}
+print(student["subject"]["chem"])
+
+# Dictionary Methods (here myDict is dictionary name)
+
+# myDict.keys() #returns all keys
+# myDict.values() #returns all values
+# myDict.items() #returns all (key, val) pairs as tuples
+# myDict.get( "key"" ) #returns the key according to value
+# myDict.update( newDict ) #inserts the specified items to the dictionary
+
+# new_dict = {"city":"delhi"}
+# student.upadte(new_dict)
