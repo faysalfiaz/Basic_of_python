b9e7aaf90b4252068686ac48d8efd12d22d3bd53
faysalfiaz <faysal15961@gmail.com>
2026-09-21 22:22:34 -0700

    add a comment & code

--- set.py
+++ set.py
@@ -2,6 +2,19 @@
 marks = {10,52,32,10,41,25,31,12}
 print(len(marks),marks)
 
+# unorder!!
 # print the value
+
+# set mutable but element isnot mutable **
 for score in marks :
     print(score)
+
+
+collection = set() # empty set   
+# set.add( el ) #adds an element
+# set.remove( el ) #removes the elem an
+# set.clear() #empties the set
+# set.pop() #removes a random value
+
+# set.union( set2 ) #combines both set values & returns new
+# set.intersection( set2 ) #combines common values & returns new
