e924327f3511f94120baa108c38fe903323f79ab
faysalfiaz <faysal15961@gmail.com>
2026-09-21 22:46:04 -0700

    add some line

--- forloops.py
+++ forloops.py
@@ -7,4 +7,14 @@ for i in num :
 print("Second way")
 for i in range (2, 21, 2):
     print(i)
-    
+
+
+# print(range(5))   
+#  for el in range(5):
+#     print(el)
+
+# for el in range(1, 5):
+#     print(el)
+
+# for el in range(1, 5, 2):
+#     print(el)
